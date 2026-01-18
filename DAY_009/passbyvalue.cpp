712e5609bb3a9b57c4dae9180e0e79e832066a48
prathamesh <prathamesh9b57@gmail.com>
2026-01-18 17:48:09 +0530

    DSA

--- DAY_009/passbyvalue.cpp
+++ DAY_009/passbyvalue.cpp
@@ -6,7 +6,7 @@ void changeA(int a){
     cout<< "function :"<< a << "\n";
 }
 int main(){
-
+    // PASS BY VALUE: when parameter is a copy of actual argument variable in memory.
     int a=10;
     changeA(a);
     cout<< "main function :" << a << "\n";
