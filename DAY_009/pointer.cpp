4bcde465bd0345672980187e456fcbbb7030b340
prathamesh <prathamesh9b57@gmail.com>
2026-01-18 17:29:49 +0530

    DSA

--- DAY_009/pointer.cpp
+++ DAY_009/pointer.cpp
@@ -8,5 +8,11 @@ int main(){
     cout<<"&a: " << &a << "\n"; //0x
     cout<<"ptr: " << ptr << "\n";   //0x
     cout<<"*ptr: " << *ptr << "\n"; //10   :dereference operator
+
+    int *ptr2 = NULL;
+    cout << "ptr2: " << ptr2 << "\n";
+    cout << "*ptr2: " << *ptr2 << "\n";    //segmentation fault : never dereference null pointer
+    cout<< "will not print." ; 
+
     return 0;
 }
