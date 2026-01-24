832e6c734a34ccfce9bc8a7aa8ee0a91ee68e191
prathamesh <prathamesh9b57@gmail.com>
2026-01-24 09:53:41 +0530

    DSA

---
+++ DAY_011/maxsubarray-2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bruteforce(int *arr,int n){
+    int maxSum =INT_MIN;
+    for(int i = 0 ; i < n ; i++){
+        int currSum=0;
+        for(int j = i ; j < n ; j++){
+            currSum += arr[j];  
+            maxSum = max(maxSum , currSum);
+        }   
+    }
+    cout <<"max subarray sum is :" << maxSum<< "\n";
+
+}
+
+int main() {
+    int arr[6] = {2,-3,6,-5,4,2};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bruteforce(arr,size);
+    return 0;
+}
