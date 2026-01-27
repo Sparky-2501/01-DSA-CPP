7882ab4be1bc3ef266815918f0fe069926ae2a8e
prathamesh <prathamesh9b57@gmail.com>
2026-01-27 21:25:02 +0530

    DSA

--- DAY_011/trappingwater.cpp
+++ DAY_011/trappingwater.cpp
@@ -19,6 +19,14 @@ void call(int *heights,int n){
         righttmax[i]= max(righttmax[i+1],heights[i+1]);
         cout<<righttmax[i]<<" ";
     }
+    int trappedwater=0;
+    for(int i=0 ; i< n ; i++){
+        int waterlevel= min(leftmax[i],righttmax[i]);
+        if(waterlevel > heights[i]){
+            trappedwater += waterlevel - heights[i];
+        }
+    }
+    cout<< "\ntrapped water: "<< trappedwater <<'\n';
 }
 
 int main() {
@@ -28,6 +36,7 @@ int main() {
     //bar2= right max
     //min(bar1 , bar2) - bar3 = trapped water
     int heights[] ={4,2,0,6,3,2,5};
+    //6+5=11
     int n = sizeof(heights)/sizeof(heights[0]);
     call(heights,n);
     return 0;
