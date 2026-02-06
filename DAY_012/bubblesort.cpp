9bd9f2d7e50278de42b4160597e035ea4fa42078
prathamesh <prathamesh9b57@gmail.com>
2026-02-06 20:17:01 +0530

    DSA

--- DAY_012/bubblesort.cpp
+++ DAY_012/bubblesort.cpp
@@ -11,11 +11,17 @@ int main() {
     //bubblesort
     int arr[] = {5, 1, 4, 2, 8,12,67,45,23,90,67};
     int n = sizeof(arr)/sizeof(arr[0]);
+
+    bool isTrue = false;
     for(int i=0 ; i<n-1 ; i++){
         for(int j=0 ; j<n-i-1 ; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                isTrue = true;
             }
+        }
+        if(isTrue == false){
+            break;
         }
     }
     for(int i=0 ; i<n ; i++){
