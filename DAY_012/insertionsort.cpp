282bed05ba95c4cd68508cbb4c6f9857f56f39f1
prathamesh <prathamesh9b57@gmail.com>
2026-02-07 00:03:56 +0530

    DSA

--- DAY_012/insertionsort.cpp
+++ DAY_012/insertionsort.cpp
@@ -4,12 +4,17 @@ using namespace std;
 int insertionsort(int arr[],int n){
     for(int i=1 ; i<n ; i++){
         int key = arr[i];
-        int j = i-1;
-        while(j>=0 && arr[j] > key){
-            arr[j+1] = arr[j];
-            j--;
+        int prev = i-1;
+        while(prev>=0 && arr[prev] > key){
+
+            /*-------------------------------*/
+            arr[prev+1] = arr[prev];
+            //          or
+            //swap(arr[prev],arr[prev+1]);
+            /*-------------------------------*/
+            prev--;
         }
-        arr[j+1] = key;
+        arr[prev+1] = key;
     }
     for(int i=0 ; i<n ; i++){
         cout<<arr[i]<<" ";
