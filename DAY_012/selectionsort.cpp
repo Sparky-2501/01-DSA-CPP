51dff75e83743c769c06580fc2ebcd8188074e15
prathamesh <prathamesh9b57@gmail.com>
2026-02-06 21:15:30 +0530

    DSA

--- DAY_012/selectionsort.cpp
+++ DAY_012/selectionsort.cpp
@@ -9,7 +9,7 @@ int main() {
         for (int j = i+1; j < n; j++)
             if (arr[j] < arr[min_idx])
                 min_idx = j;
-        swap(arr[min_idx], arr[i]);
+        swap(arr[min_idx], arr[i]); 
     }
     for (int i = 0; i < n; i++)
     {
