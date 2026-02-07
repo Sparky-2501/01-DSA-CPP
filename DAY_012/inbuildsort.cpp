e9d07a18dbedcaa7fb30e289fc5fe8e994551306
prathamesh <prathamesh9b57@gmail.com>
2026-02-07 08:02:24 +0530

    DSA

--- DAY_012/inbuildsort.cpp
+++ DAY_012/inbuildsort.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    //inbuild sort
+int main()
+{
+    // 1]ascending
     vector<int> v = {5, 2, 9, 1, 5, 6};
     sort(v.begin(), v.end());
-    for (int num : v) {
+    for (int num : v)
+    {
+        cout << num << " ";
+    }
+    cout<< endl;
+
+    //2]Descending
+    sort(v.begin(), v.end() ,greater<int>());
+    for (int num : v)
+    {
         cout << num << " ";
     }
     return 0;
---
+++ DAY_012/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+int arr[5] = {41,22,93,74,35};
+    // sort(arr,arr+5);
+    // for(int i=0 ; i< 5 ; i++){
+    //     cout<< arr << " ";
+    // }
