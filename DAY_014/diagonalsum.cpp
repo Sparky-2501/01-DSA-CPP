ee7517cc7b58327f26ad9568dbeac5e1e0a209c6
prathamesh <prathamesh9b57@gmail.com>
2026-02-09 18:33:09 +0530

    DSA

--- DAY_014/diagonalsum.cpp
+++ DAY_014/diagonalsum.cpp
@@ -12,19 +12,19 @@ int main() {
     int fd=0,sd=0,common=0;
     for(int i= 0 ; i < row ; i++){
         for(int j=0 ; j< col ; j++){
-            if(i == j && i+j == row-1){
+            if(i == j || i+j == row-1){
                 fd +=arr[i][j];
-                cout<< i << "," <<j << ":" << arr[i][j] <<  "\n ";
+                            // cout<< i << "," <<j << ":" << arr[i][j] <<  "\n ";
             }
-            // if(i+j == row-1){
-            //     sd += arr[i][j];
-            //     cout<< i<< "," << j << ":" << arr[i][j] <<  "\n ";
-            // }
+                            // if(i+j == row-1){
+                            //     sd += arr[i][j];
+                            //     cout<< i<< "," << j << ":" << arr[i][j] <<  "\n ";
+                            // }
             if(row %2 != 0){
                 common = arr[i/2][j/2];
             }
         }
     }
-    cout<< "diagonal sum is:"<< fd+sd-common << "\n";
+    cout<< "diagonal sum is:"<< fd-common << "\n";
     return 0;
 }
