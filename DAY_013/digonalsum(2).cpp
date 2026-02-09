bd6c87ed554485fc8704e96680de26e4f257af69
prathamesh <prathamesh9b57@gmail.com>
2026-02-09 18:39:14 +0530

    DSA

---
+++ DAY_013/digonalsum(2).cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[4][4] = {
+        {1, 2, 3, 10},
+        {4, 5, 6, 11},
+        {7, 8, 9, 12},
+        {13, 14, 15, 16}};
+    int row = 4, col = 4, fd = 0, sd = 0, common = 0;
+    return 0;
+}
