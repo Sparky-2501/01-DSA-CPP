6facd1006e8d109ab9f86e4a053bcec034e68b85
prathamesh <prathamesh9b57@gmail.com>
2026-02-10 20:25:50 +0530

    DSA

--- DAY_013/2Darray-2.cpp
+++ DAY_013/2Darray-2.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main() {
     int arr[2][2] = {{1,2},{3,4}};
-    
-
+    cout << arr << "==" << &arr[0][0] << "\n";
+    cout << arr+1 << "!=" << &arr[0][1] << "\n";
+    cout << arr+1 << "==" << &arr[1][0] << "\n";
     return 0;
 }
