a125389db738204fd08f3e228ceffc716cdf6667
prathamesh <prathamesh9b57@gmail.com>
2026-02-10 20:26:57 +0530

    DSA

--- DAY_013/2Darray-2.cpp
+++ DAY_013/2Darray-2.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
     int arr[2][2] = {{1,2},{3,4}};
-    cout << arr << "==" << &arr[0][0] << "\n";
+    cout << arr << "==" << &arr[0][0] << "\n";      // can print only first element but in real points all the row
     cout << arr+1 << "!=" << &arr[0][1] << "\n";
-    cout << arr+1 << "==" << &arr[1][0] << "\n";
+    cout << arr+1 << "==" << &arr[1][0] << "\n";    // when we increase the arr+1 it goes to next row
     return 0;
 }
