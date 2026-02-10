eacc4797271e6ea8ff91d816ca3aff16852f8281
prathamesh <prathamesh9b57@gmail.com>
2026-02-10 19:39:14 +0530

    DSA

---
+++ DAY_013/staircasesearch.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[4][4] = {{1,2,3,4},
+                     {11,12,13,14},
+                     {21,22,23,24},
+                     {31,32,33,34} }
+    return 0;
+}
