237c0a83a9a0c793d4ff6a4aefa52fa1cee09148
prathamesh <prathamesh9b57@gmail.com>
2026-02-10 20:05:03 +0530

    DSA

--- DAY_013/staircasesearch.cpp
+++ DAY_013/staircasesearch.cpp
@@ -1,10 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int arr[4][4] = {{1,2,3,4},
-                     {11,12,13,14},
-                     {21,22,23,24},
-                     {31,32,33,34} }
+void search1(int arr[4][4], int n, int m)
+{
+    int target = 31;
+    int i = 0, j = m - 1;
+    for (int k = 0; k < n * 2; k++)
+    {
+        if (arr[i][j] == target)
+        {
+            cout << arr[i][j];
+            return;
+        }
+        else if (arr[i][j] > target)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    cout << "not found";
+}
+
+void search2(int arr[4][4], int n, int m)
+{
+    int target = 31;
+    int i = 0, j = m - 1;
+    while (i < n && j >= 0)
+    {
+        if (arr[i][j] == target)
+        {
+            cout << arr[i][j];
+            return;
+        }
+        else if (arr[i][j] > target)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    cout << "not found";
+}
+
+int main()
+{
+    int arr[4][4] = {{1, 2, 3, 4},
+                     {11, 12, 13, 14},
+                     {21, 22, 23, 24},
+                     {31, 32, 33, 34}};
+    search1(arr, 4, 4);
     return 0;
 }
