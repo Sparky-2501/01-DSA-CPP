2682c2834f794445cf106736069cdc6e2c97d6c4
prathamesh <prathamesh9b57@gmail.com>
2026-02-15 07:28:21 +0530

    DSA

--- DAY_014/charArray-1.cpp
+++ DAY_014/charArray-1.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+//CHAR
+        char ch1 = 'a';
+        char ch2 = 'A';
+        char ch3 = '@';
+        char ch4 = '\n';
+    // takes space of 1 byte
+    // ASCII : american standard code for information interchange
+    //  'a' => 97
+    //  'A' => 65
+//CHARACTER ARRAY
+        char str[6] = {'a','b','c','d','e'};
+        cout << str << "\n";
+        
+        char arr[10] = {'c','o', 'd' , 'e' , '\0'};
+        cout << arr << "\n";
     return 0;
 }
