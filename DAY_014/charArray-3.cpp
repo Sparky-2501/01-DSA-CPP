e31314f6e9796c5ed49f856f120d2f9a125e5d43
prathamesh <prathamesh9b57@gmail.com>
2026-02-17 20:21:52 +0530

    DSA

--- DAY_014/charArray-3.cpp
+++ DAY_014/charArray-3.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    //reverse an array 
-    
+void swap(int num1, int num2)
+{
+}
+
+int main()
+{
+    // reverse an array
+    char str[] = "codef";
+    // cout << strlen(str);
+    int i = 0, j = strlen(str) - 1;
+
+    while (j >= i)
+    {
+        int temp =str[i] ;
+        str[i]= str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+    cout << str;
     return 0;
 }
