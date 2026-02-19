bca9a614ea7cf272cae0b6c2efef403287fbe05a
prathamesh <prathamesh9b57@gmail.com>
2026-02-19 19:45:08 +0530

    DSA

--- DAY_001/hello.cpp
+++ DAY_001/hello.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
     cout<<"Hello world!"<<endl;
+    cout<< "first program."<< "\n";
     return 0;
 }
--- DAY_002/unaryoerator.cpp
+++ DAY_002/unaryoerator.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main(){
+    
     int a=10;
     int b= ++a; 
     cout<<"Value of b after pre-incrementing a: "<<b<<endl;
     
-    a=10;
-    b=a++;
-    cout<<"Value of b after post-incrementing a: "<<b<<endl;
+    c=10;
+    d=c++;
+    cout<<"Value of d after post-incrementing a: "<<d<<endl;
 
     cout<<'A'+ b++ +1<<endl;           
     cout<<'A'+ ++b +1<<endl;  
