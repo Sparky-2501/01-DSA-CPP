0d0635b4b3a0e91682c62546b5408be87f41620a
prathamesh <prathamesh9b57@gmail.com>
2026-02-22 07:49:48 +0530

    DSA

--- DAY_014/cstring.cpp
+++ DAY_014/cstring.cpp
@@ -9,8 +9,16 @@ int main()
     strcpy(str2, str1);
     // cout << str1 << "\n" << str2;
 
-    char str3[100] = " successful future";
-    strcat(str2,str3);
-    cout << str1 << "\n" << str2 << "\n" << str3 << "\n";
+    char str3[100] = " successful,bright future";
+    strcat(str3,str2);
+   //cout << str1 << "\n" << str2 << "\n" << str3 << "\n";
+   
+    //str1 => healthy dsa
+    //str2 => healthy dsa
+    //str3 => succ fututre healthy dsa
+    cout << strcmp(str1,str2) << "\n"; //0
+    cout << strcmp(str2,str3) << "\n"; //+ve
+    cout << strcmp(str3,str2) << "\n"; //-ve
+    cout << strcmp(str1,str3) << "\n"; //+ve
     return 0;
 }
