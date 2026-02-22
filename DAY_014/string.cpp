719c68836b6826003827de9383f17d11d2e91b68
prathamesh <prathamesh9b57@gmail.com>
2026-02-22 17:25:48 +0530

    DSA

--- DAY_014/string.cpp
+++ DAY_014/string.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
+    // string str = "oops concept";
+    // cout << str.length()<< endl;
+    // cout<<str.at(2) << endl;
+    // cout<<str[2] << endl;
     
+    string str2;
+    getline(cin,str2);
+    cout<<str2.at(5);
     return 0;
 }
