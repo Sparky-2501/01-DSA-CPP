c3d84ef571c966075e5b5357033c565c3bc4ead0
prathamesh <prathamesh9b57@gmail.com>
2026-02-22 07:36:42 +0530

    DSA

---
+++ DAY_014/cstring.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char str1[100];
+    char str2[100];
+    strcpy(str1,"coding is passion");
+    strcpy(str2,str1);
+    cout << str1 << "\n" << str2;
+
+    return 0;
+}
