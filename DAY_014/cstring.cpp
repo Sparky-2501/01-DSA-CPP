10f957058bc1b6aea87d6548482139c76c3f212e
prathamesh <prathamesh9b57@gmail.com>
2026-02-22 07:41:07 +0530

    DSA

--- DAY_014/cstring.cpp
+++ DAY_014/cstring.cpp
@@ -1,12 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     char str1[100];
     char str2[100];
-    strcpy(str1,"coding is passion");
-    strcpy(str2,str1);
-    cout << str1 << "\n" << str2;
+    strcpy(str1, "healthy DSA");
+    strcpy(str2, str1);
+    // cout << str1 << "\n" << str2;
 
+    char str3[100] = " successful future";
+    strcat(str2,str3);
+    cout << str1 << "\n" << str2 << "\n" << str3 << "\n";
     return 0;
 }
