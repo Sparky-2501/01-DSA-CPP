a88a8546e87b6fcb4ced6025212f9d7d2e0e85db
prathamesh <prathamesh9b57@gmail.com>
2026-02-25 07:59:20 +0530

    DSA

--- vector5.cpp
+++ vector5.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
-    
+    vector <int> vec = {1,2,3,4};
+    cout<< vec.size()<<"\n";    //4
+    cout<< vec.capacity()<<"\n";    //4
+
+    vec.push_back(5);
+    cout<< vec.size()<<"\n";    //5
+    cout<< vec.capacity()<<"\n";    //8
+
+    vec.push_back(6);
+    vec.push_back(7);
+    vec.push_back(8);
+    vec.push_back(9);
+    cout<< vec.size()<<"\n";    //9
+    cout<< vec.capacity()<<"\n";    //16
     return 0;
 }
