e20d2f9e793a2a3bba56275d8a61bfa949c9b5e2
prathamesh <prathamesh9b57@gmail.com>
2026-02-28 07:21:28 +0530

    dsa

---
+++ DAY_015/sizecapacity.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector <int> vec;
+    for(int i=0 ; i< 5 ; i++){
+        vec.push_back(i);
+    }
+    cout << "Size: " << vec.size() << endl;
+    cout << "Capacity: " << vec.capacity() << endl;
+    return 0;
+}
+/*
+i   size   capacity
+0   1      1    |1|
+1   2      2    |1|2|  {if full doub}
+2   3      4    |1|2|3|_|
+3   4      4    |1|2|3|4|
+4   5      10   |1|2|3|4|5|_|_|_|_|_|
+*/
