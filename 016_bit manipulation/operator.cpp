22292714268f52814ebde7d3b29326ddc03f9c1c
prathamesh <prathamesh9b57@gmail.com>
2026-03-04 18:57:49 +0530

    dsa

---
+++ 016_bit manipulation/operator.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //&& - logical AND
+    //& - bitwise AND
+    // | - bitwise OR
+    // ^ - bitwise XOR
+    // 3 & 5 = 011 & 101 = 001 = 1  { 00=0 01=0 10=0 11=1 }
+    // 3 | 5 = 011 | 101 = 111 = 7  { 00=0 01=1 10=1 11=1 }
+    // 3 ^ 5 = 011 ^ 101 = 110 = 6  { 00=0 01=1 10=1 11=0 }
+    return 0;
+}
