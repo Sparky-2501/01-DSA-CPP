ff21d24d30205747e1e7e8d7dff8833cdaefb974
prathamesh <prathamesh9b57@gmail.com>
2026-03-05 05:50:38 +0530

    dsa

---
+++ 016_bit manipulation/compliment.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    /*
+    binary not operator
+    ~1 = 0
+    ~0 = 1
+    ~5 = ~(0000 0101) = 1111 1010 = -6
+    */
+//    int num=5;
+//    cout<< ~num<<endl;
+
+   for(int i = 0 ; i<5 ; i++){
+       cout<< i << ":" << ~i << " \n";
+   }
+    return 0;
+}
---
+++ 016_bit manipulation/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+int num=5;
+//    cout<< ~num<<endl;
