c1499b5890effbc18b55a19af333f1fa6770e28f
prathamesh <prathamesh9b57@gmail.com>
2026-03-11 06:46:23 +0530

    dsa

---
+++ 016_bit manipulation/getith.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    /*
+    Get the ith bit of a number n
+    n = 5 (0101)
+    i = 0 -> 1
+    i = 1 -> 0
+    i = 2 -> 1  
+    */
+    return 0;
+}
