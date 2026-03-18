03c44fc257f403a8529be3c0c03fa00e3bbc1d2e
prathamesh <prathamesh9b57@gmail.com>
2026-03-18 17:47:20 +0530

    dsa

--- 016_bit manipulation/checkforpowerof2.cpp
+++ 016_bit manipulation/checkforpowerof2.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    
+    int arr[] = {1, 2, 4, 8, 16,8};
+    int ans=0;
+    for(int i=0 ; i<6;i++){
+        ans =arr[i] ^ ans;
+    }
+    cout << ans<<endl;
+
     return 0;
 }
