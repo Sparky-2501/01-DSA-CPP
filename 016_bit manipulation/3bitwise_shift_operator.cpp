5bc0ed7c256d483822b5a7e6e142a1c2201407df
prathamesh <prathamesh9b57@gmail.com>
2026-03-18 19:38:33 +0530

    dsa

---
+++ 016_bit manipulation/3bitwise_shift_operator.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    /*
+    left shift operator (<<)
+    5<<1 = 10
+    5<<2 = 20
+    right shift operator (>>)
+    5>>1 = 2
+    5>>2 = 1
+
+    */
+   cout<< (5<<1) <<endl; // 10
+   cout<< (5<<2) <<endl; // 20
+   cout<< (10>>1) <<endl; // 5
+   cout<< (5>>2) <<endl; // 1
+    return 0;
+}
