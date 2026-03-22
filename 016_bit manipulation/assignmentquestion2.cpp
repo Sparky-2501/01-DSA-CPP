0ba8477f622948e8b4c6c52c19c9239d873e09da
prathamesh <prathamesh9b57@gmail.com>
2026-03-22 21:24:30 +0530

    DSA

--- 016_bit manipulation/assignmentquestion2.cpp
+++ 016_bit manipulation/assignmentquestion2.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
-    
+    int n=7,dig=2;
+
+    for(int i=0 ; i < dig ; i++){
+        n = n & ~(1 << i);
+    }
+    cout << n << endl;
     return 0;
 }
+
+/*
+i   n       1<<i    ~(1<<i)   n & ~(1<<i)
+0   0111   0001       1110      0110
+1   0110   0010       1101      0100
+2   0100   0100       1011      0000
+
+*/
