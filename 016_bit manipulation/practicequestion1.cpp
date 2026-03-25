8e8e963afdf47adcf8e76e160df59c1f0c0cf81c
prathamesh <prathamesh9b57@gmail.com>
2026-03-25 18:52:30 +0530

    DSA

--- 016_bit manipulation/practicequestion1.cpp
+++ 016_bit manipulation/practicequestion1.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Write a Function to clear the range of bits from i to j in a given number. (i & j are counted backwards from the right end of the number)
-int clear(int num, int i, int j)
+void clearmethod1(int num, int i, int j)
 {
     for (int k = i; k <= j; k++)
     {
         num = num & ~(1 << k);
     }
-    return num;
+    cout << num;
+}
+
+void clearmethod1(int num, int i, int j)
+{
+   int a = ~0 << (j + 1); // left part of the number with 0s from j to 0
+    int b = (1 << i) - 1; // right part of the number with 0s from i to end
+    num = num & (a | b);
+    cout << num;
+
 }
 
 int main()
 {
     int num, i, j;
     cin >> num >> i >> j;
-    int res = clear(num, i, j);
-    cout << res;
+    clearmethod1(num, i, j);
+    clearmethod2(num, i, j);
     return 0;
 }
