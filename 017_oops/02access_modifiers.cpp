d20156233f2f85c185f045a9416326e3273cc280
prathamesh <prathamesh9b57@gmail.com>
2026-03-31 06:58:21 +0530

    DSA

--- 017_oops/02access_modifiers.cpp
+++ 017_oops/02access_modifiers.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class A {
+    public:
+        int x; //public member
+    private:
+        int y; //private member
+    protected:
+        int z; //protected member
+};
+
 int main() {
     //access modifiers
     //1.public: members are accessible from outside the class.  
     //2.private: members are not accessible from outside the class. they can only be accessed by member functions of the class.
     //3.protected: members are not accessible from outside the class. they can only be accessed by member functions of the class and derived classes.   
     
+    A a;
+    a.x = 10; //allowed
+    //a.y = 20; //not allowed
+    //a.z = 30; //not allowed
     return 0;
 }
