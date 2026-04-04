17e6051da1b1e9fdebf613324c7b69aff225f3d7
prathamesh <prathamesh9b57@gmail.com>
2026-04-04 06:43:55 +0530

    DSA

--- 017_oops/05constructor2.cpp
+++ 017_oops/05constructor2.cpp
@@ -1,7 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+class Car
+{
+    string name;
+    string color;
+
+public:
+    Car(string n, string c)
+    {
+        name = n;
+        color = c;
+    }
+
+    void start()
+    {
+        cout << "Car Started" << endl;
+    }
+    void stop()
+    {
+        cout << "Car Stopped" << endl;
+    }
+
+    string getName(){
+        return name;
+    }
+    string getColor(){
+        return color;
+    }
+
+};
+
+//getter-get value from class ; setter - set value to class 
+int main()
+{
+    Car r1("BMW", "Red");
+    cout << r1.getName() << endl;
+    cout << r1.getColor() << endl;
     return 0;
 }
