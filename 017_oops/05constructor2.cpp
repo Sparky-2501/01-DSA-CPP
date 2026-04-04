a4268e8eca66920ed4d62d36a5f04460fa8ac7c4
prathamesh <prathamesh9b57@gmail.com>
2026-04-04 06:53:25 +0530

    DSA

--- 017_oops/05constructor2.cpp
+++ 017_oops/05constructor2.cpp
@@ -11,7 +11,7 @@ public:
     }
     Employee(int id)
     {                  // parameterized constructor, called when arguments are passed during object creation
-        this->id = id; // using 'this' pointer to refer to the current object's id
+        this->id = id; // using 'this' pointer to refer to the current object's id 
     }
     void setId(int id)
     {                  // public member function to set the value of id
@@ -25,9 +25,13 @@ public:
 
 int main()
 {
+    Employee e;
+    cout << e.getId() << endl; // Output: 0, default constructor initializes id to 0
+    Employee e1(3);
+    cout << e1.getId() << endl; // Output: 3
     /* A special method that is automatically called when an object of the class is created
     used for initialization of properties of an object
-
+    
     same name as class
     doesn't have return type
     only called once at creation of object
