d851998a67b897c211741182bdd7e584a6a31642
prathamesh <prathamesh9b57@gmail.com>
2026-04-07 05:44:47 +0530

    dsa

--- 017_oops/07copyconstructor.cpp
+++ 017_oops/07copyconstructor.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+class A {
+    public:
+        int x;
+        A(int x) {
+            this->x = x;
+        }
+        A(A &a) {
+            this->x = a.x;
+        }
+    };
     
+int main() {
+    // copy constructor is used to create a new object from an existing object
+    // it is a constructor which takes an object of the same class as a parameter
+    
+    A obj1(5);
+    A obj2(obj1); // copy constructor is called here
+    cout << obj1.x << " " << obj2.x << endl; // 5 5
     return 0;
 }
