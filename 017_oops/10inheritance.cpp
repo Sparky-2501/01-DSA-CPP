34b4c181210e8755d72300431d7246248b9b4db9
prathamesh <prathamesh9b57@gmail.com>
2026-04-11 07:32:39 +0530

    dsa

--- 017_oops/10inheritance.cpp
+++ 017_oops/10inheritance.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
-    
+    // Inheritance is a fundamental object-oriented programming concept that allows a new class (called a derived class or child class) to inherit properties and behaviors (data members and member functions) from an existing class (called a base class or parent class).
+    // This promotes code reusability and establishes a natural hierarchical relationship between classes.
     return 0;
 }
