7f99c94936243dca8dc0c4c0fc01a501d7d5cc1c
prathamesh <prathamesh9b57@gmail.com>
2026-04-14 09:03:45 +0530

    dsa

--- 017_oops/09destructor.cpp
+++ 017_oops/09destructor.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class A {
+public:
+    A() {
+        cout << "Constructor is called" << endl;
+    }
+    ~A() {
+        cout << "Destructor is called" << endl;
+    }
+};
+
 int main() {
-    
+    A obj;  
     return 0;
 }
