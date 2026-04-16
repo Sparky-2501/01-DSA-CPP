e4af312e1a8c153a15420d3ce7a3d679342c05ac
prathamesh <prathamesh9b57@gmail.com>
2026-04-16 05:40:58 +0530

    dsa

--- 017_oops/12single_inheritance.cpp
+++ 017_oops/12single_inheritance.cpp
@@ -1,7 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+class Parent
+{
+public:
+    void display()
+    {
+        cout << "I am a parent class" << endl;
+    }
+};
+class Child : public Parent
+{
+public:
+    void displayChild()
+    {
+        cout << "I am a child class" << endl;
+    }
+};
+
+int main()
+{
+    // 1 parent 1 child class
+    // child class can access parent class members
+    Child c;
+    c.display();      // Calls parent's display function
+    c.displayChild(); // Calls child's displayChild function
     return 0;
 }
