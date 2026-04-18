5f1e9ffe30dbf734ae42a7a5da62f4728aad408f
prathamesh <prathamesh9b57@gmail.com>
2026-04-19 04:29:42 +0530

    dsa

--- 017_oops/11mode_of_inheritance2.cpp
+++ 017_oops/11mode_of_inheritance2.cpp
@@ -1,13 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+class Parent
+{
+protected: // Protected-> access modifier for mode of inheritance
+    int password = 1234;
+    Parent()
+    {
+        cout << password << endl;
+    }
+};
+
+class child : public Parent
+{
+public:
+    void displayPassword()
+    {
+        cout << "Password: " << password << endl; // Child class can access protected members of parent class
+    }
+};
+
+int main()
+{
     // Inheritance can be categorized into different types based on how the classes are related to each other. The main types of inheritance are:
     // 1. Single Inheritance: A derived class inherits from a single base class.
     // 2. Multiple Inheritance: A derived class inherits from multiple base classes.
     // 3. Multilevel Inheritance: A derived class inherits from a base class, which in turn inherits from another base class.
     // 4. Hierarchical Inheritance: Multiple derived classes inherit from a single base class.
     // 5. Hybrid Inheritance: A combination of two or more types of inheritance.
-    
+    child c;
+    c.displayPassword();
     return 0;
 }
