754acd31d2537f465a5843517de79f2930daaaad
prathamesh <prathamesh9b57@gmail.com>
2026-04-23 06:28:18 +0530

    dsa

---
+++ 017_oops/14multiple_inheritance.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Base1 {
+public:
+int a = 10;
+    Base1() {
+        cout << "Base1 constructor called" << endl;
+    }
+    void display() {
+        cout << "Base1 display function" << endl;
+    }
+};
+class Base2{
+    public:
+    int b=20;
+    Base2() {
+        cout << "Base2 constructor called" << endl;
+    }
+    void display() {
+        cout << "Base2 display function" << endl;
+    }
+
+};
+
+class Derived : public Base1, public Base2 {
+public:
+    Derived() {
+        cout << a + b << endl;
+    }
+
+    void display() {
+        cout << "Derived display function" << endl;
+    }
+};
+
+
+int main() {
+    // Multiple Inheritance: A class can inherit from more than one base class.
+    Derived d;
+    d.display(); // Calls Base1's display function
+    return 0;
+}
