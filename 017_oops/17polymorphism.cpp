41deee1052e1b791ab855531d1d4be0e87690ec6
prathamesh <prathamesh9b57@gmail.com>
2026-04-27 04:34:24 +0530

    dsa

---
+++ 017_oops/17polymorphism.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Polymorphism: The ability of a function, object, or operator to take on multiple forms.
+    // Types of Polymorphism:   
+    // 1. Compile-time Polymorphism: Achieved through function overloading and operator overloading.
+    // 2. Run-time Polymorphism: Achieved through inheritance and virtual functions.
+    return 0;
+}
