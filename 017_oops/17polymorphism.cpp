1f520d4af52073441807c09164de0074aa1cb52c
prathamesh <prathamesh9b57@gmail.com>
2026-04-28 05:06:47 +0530

    dsa

--- 017_oops/17polymorphism.cpp
+++ 017_oops/17polymorphism.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+class Base
+{
+public:
+    virtual void show()
+    {
+        cout << "Base class show function" << endl;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    void show() override
+    {
+        cout << "Derived class show function" << endl;
+    }
+};
+
+int main()
+{
     // Polymorphism: The ability of a function, object, or operator to take on multiple forms.
-    // Types of Polymorphism:   
+    // Types of Polymorphism:
     // 1. Compile-time Polymorphism: Achieved through function overloading and operator overloading.
     // 2. Run-time Polymorphism: Achieved through inheritance and virtual functions.
+    Base *basePtr; // Base class pointer
+    Derived derivedObj; // Derived class object
     return 0;
 }
