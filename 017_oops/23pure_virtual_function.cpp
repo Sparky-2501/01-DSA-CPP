858da3bde96930def0c92b62ff491e96dc45c0c5
prathamesh <prathamesh9b57@gmail.com>
2026-05-05 17:35:26 +0530

    dsa

--- 017_oops/23pure_virtual_function.cpp
+++ 017_oops/23pure_virtual_function.cpp
@@ -14,6 +14,12 @@ public:
 };
 
 int main() {
+    // Base b; // Error: Cannot instantiate an abstract class
+    // Base *ptr; // Error: Cannot create pointer to an abstract class
+    // ptr = new Base(); // Error: Cannot create object of an abstract class
+    // ptr->display(); // Error: Cannot call pure virtual function
+    // To use the Derived class, we can create an object of it
+    // and call the display function, which is a concrete implementation of the pure virtual function
     Derived d;
     d.display();
     return 0;
