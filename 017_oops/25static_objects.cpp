4f7244418f63744db8db70f7ec82c8679fd4f1db
prathamesh <prathamesh9b57@gmail.com>
2026-05-07 06:41:40 +0530

    dsa

---
+++ 017_oops/25static_objects.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Static objects are created at compile time and exist for the entire duration of the program. They are initialized only once and retain their value between function calls.
+    // Static objects are created using the 'static' keyword. They can be of any data type, including user-defined types (classes).
+    static int count = 0; // Static variable to count the number of times the function is called
+    return 0;
+}
