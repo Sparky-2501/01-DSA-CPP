1151d8d6a76cfc6d34ff536a378854d732db1a8c
prathamesh <prathamesh9b57@gmail.com>
2026-05-12 07:31:04 +0530

    dsa

---
+++ 017_oops/28practice_question_1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class A{
+    public:
+    A(){
+        cout<<"Constructor of A called"<<endl;
+    }
+    ~A(){
+        cout<<"Destructor of A called"<<endl;
+    }
+};
+
+class B: public A{
+    public:
+    B(){
+        cout<<"Constructor of B called"<<endl;
+    }
+    ~B(){
+        cout<<"Destructor of B called"<<endl;
+    }
+};
+
+int main() {
+    B obj;
+    return 0;
+}
