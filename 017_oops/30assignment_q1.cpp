7da250a9ab97da0b82dbee6334c58e5f34e26cb3
prathamesh <prathamesh9b57@gmail.com>
2026-05-13 07:12:23 +0530

    dsa

--- 017_oops/30assignment_q1.cpp
+++ 017_oops/30assignment_q1.cpp
@@ -1,1 +1,32 @@
-//Question1:CreateaclasstostoreComplexnumbers.Usingoperatoroverloading,createthelogictosubtractonecomplexnumberfromanother.
+//Question1:Create a class to store Complex numbers. Using operator overloading, create the logic to subtract one complex number from another.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    Complex(int r, int i){
+        real = r;
+        imag = i;
+    }
+    
+    Complex operator-(const Complex& other) {   // Overloading the subtraction operator
+        return Complex(real - other.real, imag - other.imag);  // Return a new Complex object that is the result of the subtraction
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(5, 3);
+    Complex c2(2, 1);
+    Complex c3 = c1 - c2;
+    c3.display();
+    return 0;
+}
