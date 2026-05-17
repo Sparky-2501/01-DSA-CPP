c6a6f91850e927a6ab6600d5bde14956419285cd
prathamesh <prathamesh9b57@gmail.com>
2026-05-17 07:58:30 +0530

    dsa

--- 017_oops/32assignment_q3.cpp
+++ 017_oops/32assignment_q3.cpp
@@ -1,7 +1,40 @@
+//Question3: Create a base class Person with attributes name and age. Derive a class Student from Person and add an additional attribute studentID.
+// Implement a method displayStudentInfo() in the Student class to display all details. 
+//In the main function, a Student class object will be created in this format: Student student("Alice", 20, "S12345");
 #include <bits/stdc++.h>
 using namespace std;
 
+class Person {
+protected:
+    string name;
+    int age;
+
+public:
+    Person(string n, int a) {
+        name = n;
+        age = a;
+    }
+};
+
+class Student : public Person {
+private:
+    string studentID;
+
+public:
+    Student(string n, int a, string id) : Person(n, a) {
+        studentID = id;
+    }
+
+    void displayStudentInfo() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Student ID: " << studentID << endl;
+    }
+};
+
 int main() {
-    
+    Student student("Alice", 20, "S12345");
+    student.displayStudentInfo();
+
     return 0;
 }
