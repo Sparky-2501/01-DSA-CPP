872e6c0f5c12cedfd80668eede8a45e2be9184ff
prathamesh <prathamesh9b57@gmail.com>
2026-05-20 06:31:28 +0530

    dsa

--- 018_recursion/02recursion.cpp
+++ 018_recursion/02recursion.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//printing 1 to n
+
+void Order(int n){
+    if(n==0) return;
+    Order(n-1);
+    cout<< n <<" ";
+}
 int main() {
-    
+    //recursion in memory = stack
+    int n;
+    cin >> n;
+    Order(n);
     return 0;
 }
