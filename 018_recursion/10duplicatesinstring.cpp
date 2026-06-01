56308ab6aca28268e08d174db8b9c8553896e8b3
prathamesh <prathamesh9b57@gmail.com>
2026-06-01 07:22:20 +0530

    dsa

---
+++ 018_recursion/10duplicatesinstring.cpp
@@ -0,0 +1,30 @@
+//removing duplicates in string using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+void removeDuplicates(string s, int index, string newString, unordered_set<char> &st) {
+    //base case
+    if (index == s.length()) {
+        cout << newString << endl;
+        return;
+    }
+    //rec case
+    char currentChar = s[index];
+    if (st.find(currentChar) != st.end()) {
+        //duplicate character
+        removeDuplicates(s, index + 1, newString, st);
+    } else {
+        //non duplicate character
+        newString.push_back(currentChar);
+        st.insert(currentChar);
+        removeDuplicates(s, index + 1, newString, st);
+    }
+}
+
+int main() {
+    string s;
+    cin >> s;
+    unordered_set<char> st;
+    removeDuplicates(s, 0, "", st);
+    return 0;
+}
