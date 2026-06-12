9807c3fb2020963d2074a6b56df4319e6f108dbb
prathamesh <prathamesh9b57@gmail.com>
2026-06-12 07:51:36 +0530

    dsa

--- 019_divide & conquer/01mergesortalgo.cpp
+++ 019_divide & conquer/01mergesortalgo.cpp
@@ -2,8 +2,47 @@
 using namespace std;
 
 // Merge Sort Algorithm : time complexity O(nlogn) and space complexity O(n)
+void merge(vector<int> &arr, int left, int mid, int right) {
+    vector<int> temp(right - left + 1);
+    int i = left, j = mid + 1, k = 0;
+    while (i <= mid && j <= right) {
+        if (arr[i] < arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    for (int i = left; i <= right; i++) {
+        arr[i] = temp[i - left];
+    }
+}
+
+void mergeSort(vector<int> &arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
 
 int main() {
-    
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // Merge Sort Algorithm
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
