b3fbd1919bb05d154f6de6c70af1cf9cf3ce563e
prathamesh <prathamesh9b57@gmail.com>
2026-07-22 18:21:00 +0530

    DSA

--- 019_divide & conquer/01mergesortalgo.cpp
+++ 019_divide & conquer/01mergesortalgo.cpp
@@ -10,18 +10,19 @@ using namespace std;
 void merge(vector<int> &arr, int left, int mid, int right) {
     vector<int> temp(right - left + 1);
     int i = left, j = mid + 1, k = 0;
+    
     while (i <= mid && j <= right) {
         if (arr[i] < arr[j]) {
-            temp[k++] = arr[i++];
+            temp.push_back(arr[i++]);
         } else {
-            temp[k++] = arr[j++];
+            temp.push_back(arr[j++]);
         }
     }
     while (i <= mid) {
-        temp[k++] = arr[i++];
+        temp.push_back(arr[i++]);
     }
     while (j <= right) {
-        temp[k++] = arr[j++];   
+        temp.push_back(arr[j++]);   
     }
     for (int i = left; i <= right; i++) {
         arr[i] = temp[i - left];
