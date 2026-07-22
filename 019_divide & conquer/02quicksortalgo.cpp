a73b5bf038adb7e34ecba690d561eee0f960bd4f
prathamesh <prathamesh9b57@gmail.com>
2026-07-22 20:05:51 +0530

    DSA

--- 019_divide & conquer/02quicksortalgo.cpp
+++ 019_divide & conquer/02quicksortalgo.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//time complexity O(nlogn) and space complexity O(logn)
+//worst case time complexity O(n^2) and space complexity O(logn)
+
+
 // This function takes last element as pivot, places the pivot
 // element at its correct position in sorted array, and places 
 //all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot
