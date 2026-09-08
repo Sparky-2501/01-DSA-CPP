cfc595aa0102603309c708a481b16e8be7417e78
prathamesh <prathamesh9b57@gmail.com>
2026-09-08 18:33:19 +0530

    dsa

---
+++ 022_linked list questions/04_merge_sort_on_ll.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+Node* splitAtMid(Node* head){
+    Node* slow = head;
+    Node* fast = head->next;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node* right = slow->next;
+    slow->next = NULL;
+    return right;
+}
+
+Node* merge(Node* left, Node* right){
+    List ans;
+
+    while(left != NULL && right != NULL){
+        if(left->data <= right->data){
+            ans.push_back(left->data);
+            left = left->next;
+        }
+        else{
+            ans.push_back(right->data);
+            right = right->next;
+        }
+    }
+
+    while(left != NULL){
+        ans.push_back(left->data);
+        left = left->next;
+    }
+
+    while(right != NULL){
+        ans.push_back(right->data);
+        right = right->next;
+    }
+
+    return ans.head; // Assuming List has a head pointer
+
+} 
+
+Node* mergeSort(Node* head){
+
+    if(head == NULL || head-> next == NULL){
+        return head;
+    }
+
+    Node* right = splitAtMid(head);
+
+    mergeSort(head);
+    mergeSort(right);
+    return merge(head, right);
+}
+
+int main() {
+    
+    return 0;
+}
