1c33f8cb26df4b647b5e040866999bdd468ce069
prathamesh <prathamesh9b57@gmail.com>
2026-09-17 05:16:01 +0530

    dsa

---
+++ 023_stack basics/03_stack_using_vector_&_class.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+//stack using vector & class
+
+tempelate <class T>
+class Stack{
+    vector<T> vec;
+    public:
+    void push(T x){
+        vec.push_back(x);
+    }
+    void pop(){
+        if(vec.empty()){
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        vec.pop_back();
+    }
+    T peek(){
+        if(vec.empty()){
+            cout << "Stack is empty" << endl;
+            return T(); // Return default-constructed object of type T
+        }
+        return vec.back();
+    }
+    bool isEmpty(){
+        return vec.size() == 0;
+    }
+};
+int main() {
+    Stack<int> s;  // Stck s => Stack<int> s;  // Stack of integers
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    while(!s.isEmpty()){
+        cout << s.peek() << endl;
+        s.pop();
+    }
+
+    Stack<char> s2;  // Stack of characters
+    s2.push('a');
+    s2.push('b');
+    while (!s2.isEmpty()){
+        cout << s2.peek() << endl;
+        s2.pop();
+    }
+    
+    return 0;
+}
